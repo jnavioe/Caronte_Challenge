793dbc71997cef5497b512daa99b526ca44bf791
ikerborr@gmail.com <ikerborr@gmail.com>
2024-10-26 16:08:49 +0200

    entity ok

---
+++ src/entity/activity.py
@@ -0,0 +1,7 @@
+class Submission:
+    def __init__(self, id, data_upload, grade, date_evaluated, time_tried):
+        self.id = id
+        self.data_upload = data_upload
+        self.grade = grade
+        self.date_evaluated = date_evaluated
+        self.time_tried = time_tried
