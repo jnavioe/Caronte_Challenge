d11c025a627953d3e8b96421c87e612af842705c
IkerBorr <ikerborr@outlook.com>
2024-10-27 00:38:09 +0200

    read csv

--- main.py
+++ main.py
@@ -3,103 +3,103 @@ from src.entity.activity import Activity
 from src.entity.exam import Exam
 from src.enum.exam import ExamType
 from src.entity.student import Student
-from src.CSV.csv import CsvReader
-from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+import csv
 
+SUBMISSION_FILE = "./public/trameses.csv"
+ACTIVITY_FILE = "./public/activitats.csv"
+EXAM_FILE = "./public/notes.csv"
 
-CSV_DIR = "/home/toni/Caronte_Challenge/src/entity/activity.py"
+class ProcessStudents:
+    def __init__(self, submission_file=SUBMISSION_FILE, activity_file=ACTIVITY_FILE, exam_file=EXAM_FILE):
+        self._submission_matrix = self._read_csv(submission_file)
+        self._activities_matrix = self._read_csv(activity_file)
+        self._exams_matrix = self._read_csv(exam_file)
+        self._temp_activities = {}
 
-def prova():
-     students = []
-    # Cargar los archivos CSV
-     trameses = pd.read_csv('./public/trameses.csv')
-     activitats = pd.read_csv('./public/activitats.csv')
-     examens = pd.read_csv('./public/notes.csv', delimiter=';')
+    @staticmethod
+    def default_csv_delimiter(file_path):
+        with open(file_path, 'r') as file:
+            first_line = file.readline()
+            file.seek(0)
+            dialect = csv.Sniffer().sniff(first_line)
+        return dialect.delimiter
 
-     # Obtener todos los 'userid' únicos
-     user_ids = trameses['userid'].unique()
+    @staticmethod
+    def _read_csv(file_path):
+        try:
+            delimiter = ProcessStudents.default_csv_delimiter(file_path)
+            data = pd.read_csv(file_path, delimiter=delimiter)
+            if data.empty:
+                raise ValueError(f"Error: CSV {file_path} is empty")
+            return data
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Error: file {file_path} not found.")
+        except ValueError as e:
+            raise ValueError(e)
 
-     # Iterar sobre cada 'userid' y contar trameses y exàmens
-     for user_id in user_ids:
-          # Filtrar los trameses por el 'userid'
-          trameses_filtrados = trameses[trameses['userid'] == user_id]
+    def _students_id(self):
+        return list(set(self._exams_matrix['userid'].unique()))
 
-          tramesesObj = []
-          # Objectes trameses
-          for tramesa in trameses_filtrados.values:
-               
-               id=tramesa[0]
-               fechasubida=tramesa[3]
-               nota=tramesa[7]
-               fechaNota=tramesa[5]
-               n_intentos=tramesa[7]
+    def _load_activities(self):
+        activities_dict = {}
+        for _, activity in self._activities_matrix.iterrows():
+            act = Activity(
+                id=activity["activitat_id"],
+                name=activity["activitat"],
+                classroomId=activity["aula_id"],
+                dateInit=activity["startdate"],
+                dateEnd=activity["duedate"],
+                maxGrade=activity["grade"],
+                submissions=[]
+            )
+            activities_dict[activity["activitat_id"]] = act
+        return activities_dict
 
-               tramesesObj.append(Submission(id,fechasubida,nota,fechaNota,n_intentos)) 
+    def _find_activity_by_id(self, activity_id):
+        return self._temp_activities.get(activity_id)
 
-          if trameses_filtrados.shape[0]==0:
-               continue
-          # Inicializar la lista de actividades filtradas
-          activitats_filtrades = pd.DataFrame()  # Crear un DataFrame vacío por defecto
-          
-          # Filtrar las actividades asociadas a los trameses filtrados
-          activitat_ids = trameses_filtrados['activitat_id'].unique()
-          activitats_filtrades = activitats[activitats['activitat_id'].isin(activitat_ids)]
+    def _submissions_by_user_id(self, user_id):
+        filtered_submissions = self._submission_matrix[self._submission_matrix['userid'] == user_id]
+        submissions_list = []
+        for _, submission in filtered_submissions.iterrows():
+            activity = self._find_activity_by_id(submission['activitat_id'])
+            if activity:
+                submissions_list.append(Submission(
+                    id=submission['id'],
+                    data_upload=submission['datesubmitted'],
+                    grade=submission['grade'],
+                    date_evaluated=submission['dategraded'],
+                    time_tried=submission['nevaluations'],
+                    activity=activity
+                ))
+        return submissions_list
 
-          # Contar el número total de exàmens
-          total_examenes = activitats_filtrades
+    def _exams_by_user_id(self, user_id):
+        exams_filtered = self._exams_matrix[self._exams_matrix["userid"] == user_id]
+        exams_list = []
+        for _, exam in exams_filtered.iterrows():
+            if not pd.isna(exam["P_Grade"]):
+                exams_list.append(Exam(ExamType.Midterm, exam["aula_id"], exam["P_Grade"]))
+            elif not pd.isna(exam["F_Grade"]):
+                exams_list.append(Exam(ExamType.Final, exam["aula_id"], exam["F_Grade"]))
+            elif not pd.isna(exam["R_Grade"]):
+                exams_list.append(Exam(ExamType.Makeup, exam["aula_id"], exam["R_Grade"]))
+        return exams_list
 
+    def process(self):
+        self._temp_activities = self._load_activities()
+        students_ids = self._students_id()
+        students_list = []
+        for student_id in students_ids:
+            students_list.append(Student(
+                id=student_id,
+                submissions=self._submissions_by_user_id(student_id),
+                grades=self._exams_by_user_id(student_id)
+            ))
+        return students_list
 
-
-          # Activitat objectes
-
-          activitatsObj = []
-          for activitat in activitats_filtrades.values:
-               
-               id=activitat[0]
-               nom=activitat[1]
-               aulaId=activitat[2]
-               dataInici=activitat[3]
-               dataFinal=activitat[4]
-               notaMax=activitat[5]
-               activitatsObj.append(Activity(id,nom,aulaId,dataInici,dataFinal, notaMax,tramesesObj))
-          
-          # examens
-          examens_filtrados = examens[examens['userid'] == user_id]
-          examensObj = []
-
-          #for exam in examens_filtrados:
-          for exa in examens_filtrados.values:
-              aulaId=exa[1]
-              grade_partial=exa[2]
-              grade_final=exa[4]
-              grade_recovery=exa[6]
-              
-              examensObj.append(Exam(ExamType.Midterm, aulaId, grade_partial))
-              examensObj.append(Exam(ExamType.Final, aulaId, grade_final))
-              examensObj.append(Exam(ExamType.Makeup, aulaId, grade_recovery))
-
-        #estudiante FINAL transformer
-          students.append(Student(user_id,activitatsObj,tramesesObj,examensObj))
-
-     return students 
-
-
-    
 if __name__ == "__main__":
-    csv_reader = CsvReader(CSV_DIR)
-
-    data = csv_reader.data()
-
-    clf = RandomForestClassifier(random_state=0)
-    X = [[1, 2, 3],  # 2 samples, 3 features
-         [11, 12, 13]]
-    y = [0, 1]
-    a = clf.fit(X, y)
-    b = clf.predict(X)
-    print(b)
-
-    students=prova()
-    print(students)
-
-
+    students = ProcessStudents().process()
+    for student in students:
+        student.print()
--- src/entity/submission.py
+++ src/entity/submission.py
@@ -1,7 +1,8 @@
 class Submission:
-    def __init__(self, id, data_upload, grade, date_evaluated, time_tried):
+    def __init__(self, id, data_upload, grade, date_evaluated, time_tried, activity):
         self.id = id
         self.data_upload = data_upload
         self.grade = grade
         self.date_evaluated = date_evaluated
         self.time_tried = time_tried
+        self.activity = activity
