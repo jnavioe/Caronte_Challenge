0237bc833ddbe07368d6508bb778b3da1604a8cb
jesus <j.navio.e@gmail.com>
2024-10-26 16:28:02 +0200

    entity ok

---
+++ src/CSV/entities/user.py
@@ -0,0 +1,10 @@
+class User:
+    "====Attributes===="
+    def __init__(self, id, activities, submissions, grades):
+        self.id = id
+        self.activities = activities
+        self.submissions = submissions
+        self.grades = grades
+
+    "====Methods===="
+
